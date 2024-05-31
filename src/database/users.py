096ed436ef3ec1d772ce7d7eefd2e5068183de3f
pedrosanto90 <pedro.bb.90@gmail.com>
2024-05-31 09:48:14 +0100

    adicionado metodo para guardar as passwords encriptadas na base de dados

--- src/database/users.py
+++ src/database/users.py
@@ -2,6 +2,7 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 from mysql.connector import Error
+import bcrypt
 
 load_dotenv()
 PASSWORD= os.getenv("PASSWORD")
@@ -21,9 +22,10 @@ def create_user(user_name, user_fullname, user_password):
             # Consulta SQL para inserir um novo cliente
             insert_query = 'INSERT INTO users (user_name, user_fullname, user_password) \
                 VALUES (%s, %s, %s)'
-
+            
             # TODO: adicionar bcrypt para guardar as passwords
-            record = (user_name, user_fullname, user_password)
+            hashed_password = bcrypt.hashpw(user_password.encode('utf-8'), bcrypt.gensalt())
+            record = (user_name, user_fullname, hashed_password)
 
             cursor.execute(insert_query, record)
             connection.commit()
@@ -36,3 +38,5 @@ def create_user(user_name, user_fullname, user_password):
             cursor.close()
             connection.close()
             print("Conexão ao MySQL encerrada.")
+
+create_user('pedroesanto', 'Pedro Espirito Santo', 'Pedro915624607')
