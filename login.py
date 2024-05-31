041af2ed0f9b7c421e58d27db8cd0e3e3bf67540
pedrosanto90 <pedro.bb.90@gmail.com>
2024-05-31 09:34:09 +0100

    colocado codigo de login numa funcao "login"

--- login.py
+++ login.py
@@ -2,18 +2,19 @@ import tkinter as tk
 from tkinter import PhotoImage
 from takePic import takePic
 
-root = tk.Tk()
-root.title("Norauto - Login")
-root.geometry("600x400")
-root.configure(bg="#1f286d")
+def login():
+    root = tk.Tk()
+    root.title("Norauto - Login")
+    root.geometry("600x400")
+    root.configure(bg="#1f286d")
 
-logo = PhotoImage(file="assets/logo.png")
+    logo = PhotoImage(file="assets/logo.png")
 
-canvas = tk.Canvas(root, width=logo.width(), height=logo.height(), bg="#1f286d", highlightthickness=0)
-canvas.pack(expand=True)
-canvas.create_image(0, 0, anchor="nw", image=logo)
+    canvas = tk.Canvas(root, width=logo.width(), height=logo.height(), bg="#1f286d", highlightthickness=0)
+    canvas.pack(expand=True)
+    canvas.create_image(0, 0, anchor="nw", image=logo)
 
-button_login = tk.Button(root, text="Login", command=takePic)
-button_window = canvas.create_window(logo.width()//2, (logo.height()//2)+150, anchor="center", window=button_login)
+    button_login = tk.Button(root, text="Login", command=takePic)
+    button_window = canvas.create_window(logo.width()//2, (logo.height()//2)+150, anchor="center", window=button_login)
 
-root.mainloop()
+    root.mainloop()
