1a3b7ffc6691cafba0e1a7470079dd4c25df3203
pedrosanto90 <pedro.bb.90@gmail.com>
2024-05-31 10:09:00 +0100

    adicionada funcao para login para dos utilizadores

--- src/database/users.py
+++ src/database/users.py
@@ -39,4 +39,42 @@ def create_user(user_name, user_fullname, user_password):
             connection.close()
             print("Conexão ao MySQL encerrada.")
 
-create_user('pedroesanto', 'Pedro Espirito Santo', 'Pedro915624607')
+
+def login(user_name, user_password):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='trabalho_final',
+            user='root',
+            password=PASSWORD
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute('SELECT user_password FROM users WHERE user_name = %s', (user_name,))
+            result = cursor.fetchone()
+
+            if result is not None:
+                stored_password = result[0]
+                if bcrypt.checkpw(user_password.encode('utf-8'), stored_password.encode('utf-8')):
+                    print("Login bem-sucedido.")
+                    return "Login bem-sucedido."
+                else:
+                    print("Username ou password incorretos")
+                    return "Username ou password incorretos"
+            else:
+                print("Utilizador não encontrado.")
+                return "Utilizador não encontrado."
+            
+    except Error as err:
+        print(f'Erro ao criar o utilizador: {err}')
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Conexão ao MySQL encerrada.")
+
+# Example usage
+login('pedroesanto', 'Pedro915624607')
