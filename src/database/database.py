877d17141d9086313cf6227fced6cf38d15d8385
47252JoaoFerreira <80894140+47252JoaoFerreira@users.noreply.github.com>
2024-06-07 20:15:46 +0100

    comentários no código da database

--- src/database/database.py
+++ src/database/database.py
@@ -1,43 +1,52 @@
+# Importação dos módulos e bibliotecas
 import mysql.connector
 from dotenv import load_dotenv
 import os
 
+# Carregamento das variáveis de ambiente
 load_dotenv()
 PASSWORD = os.getenv('PASSWORD')
 
+
+# Função para conectar à base de dados
 def connect():
-    mysql.connector.connect( 
+    mysql.connector.connect(
         host="localhost",
         user="root",
         passwd=PASSWORD,
         database="trabalho_final"
     )
-    print('Database connected!')
+    print('Base de dados conectada!')  # Imprime uma mensagem de sucesso
 
+
+# Função para criar a base de dados
 def create_db():
-        mydb = mysql.connector.connect( 
-            host="localhost",
-            user="root",
-            passwd=PASSWORD,
-        )
+    # Conecta ao servidor MySQL
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=PASSWORD,
+    )
 
-        mycursor = mydb.cursor()
-        mycursor.execute('CREATE DATABASE trabalho_final')
+    mycursor = mydb.cursor()  # Cria um cursor para executar comandos SQL
+    mycursor.execute('CREATE DATABASE trabalho_final')  # Cria a base de dados "trabalho_final"
 
-        print('Database created!')
+    print('Base de dados criada!')  # Imprime uma mensagem de sucesso
 
+
+# Função para criar as tabelas na base de dados
 def create_tables():
-    mydb = mysql.connector.connect( 
+    # Conecta à base de dados "trabalho_final"
+    mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         passwd=PASSWORD,
         database="trabalho_final"
     )
 
-    mycursor = mydb.cursor()
+    mycursor = mydb.cursor()  # Cria um cursor para executar comandos SQL
 
-    # creating tables
-
+    # Criação da tabela "clients"
     mycursor.execute('CREATE TABLE clients (client_id INT AUTO_INCREMENT PRIMARY KEY, \
     client_name VARCHAR(256), \
     client_address VARCHAR(256), \
@@ -46,6 +55,7 @@ def create_tables():
     client_email VARCHAR(50), \
     client_created DATE)')
 
+    # Criação da tabela "services"
     mycursor.execute('CREATE TABLE services (service_id INT AUTO_INCREMENT PRIMARY KEY, \
     service_client_id INT, \
     service_type VARCHAR(50), \
@@ -55,6 +65,7 @@ def create_tables():
     service_state VARCHAR(50), \
     FOREIGN KEY(service_client_id) REFERENCES clients(client_id))')
 
+    # Criação da tabela "payments"
     mycursor.execute('CREATE TABLE payments (payment_id INT AUTO_INCREMENT PRIMARY KEY, \
     payment_service_id INT, \
     payment_date DATE, \
@@ -63,8 +74,11 @@ def create_tables():
     payment_type VARCHAR(50), \
     FOREIGN KEY(payment_service_id) REFERENCES services(service_id))')
 
+    # Criação da tabela "users"
     mycursor.execute('CREATE TABLE users (user_id INT AUTO_INCREMENT PRIMARY KEY, \
     user_name VARCHAR(50), \
     user_fullname VARCHAR(256), \
     user_password VARCHAR(64), \
     user_role VARCHAR(50))')
+
+    print('Tabelas criadas!')  # Imprime uma mensagem de sucesso
